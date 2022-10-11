100ab7d32bd2cd82fdf9e169a716b8c6c778427a
flstkdvlpr <sartvas.ne@yahoo.com>
2022-10-11 16:44:20 +0300

    POST and regex

--- Less8/index.php
+++ Less8/index.php
@@ -1,7 +1,24 @@
-<form action="#" method="">
+<form action="#" method="POST">
     <input type="text" name="name" id="" placeholder="Введите имя"><br>
     <label for="check" >Мне уже есть 18 лет</label>
     <input type="checkbox" name="age" id="check">
     <input type="submit" name="submit" value="Вход">
 
 </form>
+
+<?
+
+if(isset($_POST['name'])){
+    $name = trim($_POST['name']);
+    if(!preg_match('&^[a-zA-Zа-яА-ёЁ]+$&u', $name)){
+        exit("Имя некорректно");
+    }
+    $age_check = $_POST['age'];
+    if($age_check == 'on'){
+        echo "Доступ разрешён";
+    } else {
+        echo "Пшёл вон,  $name";
+    }
+}
+
+?>
